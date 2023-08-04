a31834b3b061b74de84b7350771f21f4caea5859
Metehan Kaygusuz <134321420+metehan-kaygusuz@users.noreply.github.com>
2023-08-04 20:52:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,21 +10,21 @@ let MenuItems = document.getElementById("MenuItems");
       }
 
 
-    //   let ProductImg = document.getElementById("ProductImg");
-    //   let SmallImg = document.getElementsByClassName("small-img");
+      let ProductImg = document.getElementById("ProductImg");
+      let SmallImg = document.getElementsByClassName("small-img");
 
-    //   SmallImg[0].onclick = function () {
-    //     ProductImg.src = SmallImg[0].src;
-    //   };
-    //   SmallImg[1].onclick = function () {
-    //     ProductImg.src = SmallImg[1].src;
-    //   };
-    //   SmallImg[2].onclick = function () {
-    //     ProductImg.src = SmallImg[2].src;
-    //   };
-    //   SmallImg[3].onclick = function () {
-    //     ProductImg.src = SmallImg[3].src;
-    //   };
+       SmallImg[0].onclick = function () {
+       ProductImg.src = SmallImg[0].src;
+       };
+       SmallImg[1].onclick = function () {
+         ProductImg.src = SmallImg[1].src;
+       };
+       SmallImg[2].onclick = function () {
+       ProductImg.src = SmallImg[2].src;
+       };
+       SmallImg[3].onclick = function () {
+       ProductImg.src = SmallImg[3].src;
+       };
 
 
       let carts= document.querySelectorAll(".add-cart");
